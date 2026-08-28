6d9a88240e474d658b17e3c55ffa9510fb9c87dc
Radosław Kolba <Radoslaw.Kolba@um.krakow.pl>
2026-08-28 10:09:05 +0200

    update IBM API to quantum world

--- generator/generate_random.py
+++ generator/generate_random.py
@@ -1,49 +1,35 @@
 import json
 import os
 from datetime import datetime, UTC
-
 from qiskit import QuantumCircuit
 from qiskit_ibm_runtime import QiskitRuntimeService
 from qiskit_ibm_runtime import SamplerV2
 
 POOL_SIZE = 256
+numbers = []
 
 token = os.environ["IBM_TOKEN"]
-
 service = QiskitRuntimeService(
-    channel="ibm_quantum",
+    channel="ibm_quantum_platform",
     token=token
 )
-
 backend = service.least_busy(
     simulator=False,
     operational=True
 )
 
-numbers = []
-
 while len(numbers) < POOL_SIZE:
-
     qc = QuantumCircuit(8)
-
     for q in range(8):
         qc.h(q)
-
     qc.measure_all()
-
     sampler = SamplerV2(backend)
-
     job = sampler.run([qc], shots=128)
-
     result = job.result()
-
     counts = result[0].data.meas.get_counts()
-
     for bit_string in counts.keys():
-
         if len(numbers) >= POOL_SIZE:
             break
-
         numbers.append(int(bit_string, 2))
 
 payload = {
@@ -56,16 +42,8 @@ payload = {
     "numbers": numbers
 }
 
-with open(
-    "docs/random.json",
-    "w",
-    encoding="utf-8"
-) as f:
-    json.dump(
-        payload,
-        f,
-        ensure_ascii=False
-    )
+with open("docs/random.json", "w", encoding="utf-8") as f:
+    json.dump(payload, f, ensure_ascii=False)
 
 print(
     f"Wygenerowano {len(numbers)} liczb "
