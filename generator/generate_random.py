abd5a644e0f6c07edd18e49fd3932f3f7b184f01
Radosław Kolba <Radoslaw.Kolba@um.krakow.pl>
2026-08-28 11:30:40 +0200

    update generate_random.py

--- generator/generate_random.py
+++ generator/generate_random.py
@@ -2,52 +2,79 @@ import json
 import os
 from datetime import datetime, UTC
 from qiskit import QuantumCircuit
-from qiskit_ibm_runtime import QiskitRuntimeService
-from qiskit_ibm_runtime import SamplerV2
+from qiskit.transpiler import generate_preset_pass_manager
+from qiskit_ibm_runtime import (
+    QiskitRuntimeService,
+    SamplerV2 as Sampler,
+)
 
-POOL_SIZE = 256
-numbers = []
+POOL_SIZE = 100
 
-token = os.environ["IBM_TOKEN"]
-instance = os.environ.get("IBM_INSTANCE", None)
+
 service = QiskitRuntimeService(
     channel="ibm_quantum_platform",
-    token=token,
-    instance=instance
+    token=os.environ["IBM_TOKEN"],
+    instance=os.environ["IBM_INSTANCE"]
 )
 backend = service.least_busy(
     simulator=False,
     operational=True
 )
+print(f"Backend: {backend.name}")
+qc = QuantumCircuit(8)
+for q in range(8):
+    qc.h(q)
+qc.measure_all()
+pm = generate_preset_pass_manager(
+    backend=backend,
+    optimization_level=1
+)
+isa_circuit = pm.run(qc)
+sampler = Sampler(mode=backend)
+job = sampler.run(
+    [isa_circuit],
+    shots=POOL_SIZE
+)
+print("JOB:", job.job_id())
+result = job.result()
+print(result)
+numbers = []
+pub_result = result[0]
 
-while len(numbers) < POOL_SIZE:
-    qc = QuantumCircuit(8)
-    for q in range(8):
-        qc.h(q)
-    qc.measure_all()
-    sampler = SamplerV2(backend)
-    job = sampler.run([qc], shots=128)
-    result = job.result()
-    counts = result[0].data.meas.get_counts()
-    for bit_string in counts.keys():
-        if len(numbers) >= POOL_SIZE:
-            break
-        numbers.append(int(bit_string, 2))
+try:
+    counts = pub_result.data.meas.get_counts()
+    for bitstring, count in counts.items():
+        value = int(bitstring, 2)
+        for _ in range(count):
+            numbers.append(value)
+
+except Exception:
+    print("Nie udało się automatycznie odczytać wyników")
+    print(pub_result)
+    raise
 
 payload = {
     "generated_at": datetime.now(
         UTC
     ).isoformat(),
-    "backend": backend.name,
     "provider": "IBM Quantum",
+    "backend": backend.name,
     "pool_size": len(numbers),
     "numbers": numbers
 }
 
-with open("docs/random.json", "w", encoding="utf-8") as f:
-    json.dump(payload, f, ensure_ascii=False)
+with open(
+    "docs/random.json",
+    "w",
+    encoding="utf-8"
+) as f:
+    json.dump(
+        payload,
+        f,
+        indent=2,
+        ensure_ascii=False
+    )
 
 print(
-    f"Wygenerowano {len(numbers)} liczb "
-    f"na backendzie {backend.name}"
+    f"Zapisano {len(numbers)} liczb."
 )
